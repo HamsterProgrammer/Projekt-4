66ea31b980537b0eb3434ad91c0cbd270bca33c1
FlyBozon <127512238+FlyBozon@users.noreply.github.com>
2023-06-24 21:31:00 +0200

    Update Resource.h

--- WindowsProject1/Resource.h
+++ WindowsProject1/Resource.h
@@ -3,19 +3,21 @@
 // Used by WindowsProject1.rc
 
 #define IDS_APP_TITLE			103
-
 #define IDR_MAINFRAME			128
 #define IDD_WINDOWSPROJECT1_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_WINDOWSPROJECT1			107
+#define IDI_WINDOWSPROJECT1		107
 #define IDI_SMALL				108
-#define IDC_WINDOWSPROJECT1			109
+#define IDC_WINDOWSPROJECT1		109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
 #define ID_BUTTON				210
+#define IDM_CIRCLE				101
+#define IDM_TRIANGLE			111
+#define IDM_SQUARE				110
 #endif
 // Next default values for new objects
 //
